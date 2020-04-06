4e7c73c2b6b69eb5aceb190794e13afa3c43517f
Yann Bolliger <yann.bolliger@sqooba.io>
2020-04-06 15:12:40 +0200

    Updates

---
+++ precache-manifest.af9c7121c1ed86335813a241e5938308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af6ac732f2ab7d00634d0a9ba65e07f",
+    "url": "/korono/index.html"
+  },
+  {
+    "revision": "9267fde5cd4c629c1860",
+    "url": "/korono/static/js/2.48a765ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/korono/static/js/2.48a765ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f14080f4cfc51ff5eab",
+    "url": "/korono/static/js/main.e7da1c6b.chunk.js"
+  },
+  {
+    "revision": "0cb3f2c84cb1a438f3c9",
+    "url": "/korono/static/js/runtime-main.72642137.js"
+  }
+]);
